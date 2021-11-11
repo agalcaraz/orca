2af698b3053701908eae8d3abc3a834c104bfa6c
Antonio Garcia <antonio.garcia47@um.es>
2021-11-11 12:44:10 +0100

    add geom_all.py script

---
+++ geom_all.py
@@ -0,0 +1,204 @@
+﻿# -------------------------------------------------
+#
+# Script for parsing main information of a set of geometry
+# optimizations (with vibrational frequencies calculation) 
+# done in orca 4.2.1 using as input .log files
+#
+# The input format is: 'python geom_all'
+#
+# Output information: electronic parameters, vibrational
+# frequencies and energy vs nº cycles representation
+#
+# Output files: filename.txt & filename.png for every file
+# -------------------------------------------------
+# =================================================
+# Written by Antonio Garcia Alcaraz 11/2021
+# =================================================
+
+import os
+import glob
+import argparse
+import matplotlib.pyplot as plt
+import sys
+
+if __name__ == "__main__":
+    
+    #Specify what we want to read
+    file_location = os.path.join('*.log')
+    filenames = glob.glob(file_location)
+    
+    for f in filenames:
+        #Get the file name
+        file_name = os.path.basename(f)
+        split_filename = file_name.split('.')
+        fname = split_filename[0]
+        
+        #Read the data
+        outfile = open(f, 'r')
+        data = outfile.readlines()
+        outfile.close()
+        
+        #Open a file for writing
+        output_location = F'{fname}.txt'
+        output = open(output_location, 'w+')
+        
+        #Pasting main script
+        
+        #Checking if the calculation was correctly finished
+        found = False
+        for line in data:
+            if 'OPTIMIZATION RUN DONE' in line:
+                found = True
+        if not found:
+            sys.exit('Optimization not completed, something went wrong.')
+            
+        found = False
+        for line in data:
+            if 'ORCA TERMINATED NORMALLY' in line:
+                found = True
+        if not found:
+            sys.exit('Calculation not completed. Something went wrong in the termochemistry analysis.')
+                
+        print('-----------------------------------------')
+        print('Data for {}.log'.format(fname))
+        print('-----------------------------------------')
+        output.write(f'-----------------------------------------\n')
+        output.write(f'Data for {fname}.log\n')
+        output.write(f'-----------------------------------------\n')
+        
+        #Printing energy, zpe and g correction
+    
+        all_energies = []
+
+        for line in data:
+            if 'FINAL SINGLE POINT ENERGY' in line:
+                energy_line = line
+                energy_line_split = energy_line.split()
+                energy_values = float(energy_line_split[-1])
+                all_energies.append(energy_values)  
+            if 'Zero point energy' in line:
+                zero_line = line
+                zero_line_split = zero_line.split()
+                zero_value = float(zero_line_split[4])
+            if 'G-E(el)' in line:
+                g_line = line
+                g_line_split = g_line.split()
+                g_value = float(g_line_split[2])
+
+        print('Final single point energy (Eh) : {}'.format(all_energies[-1]))
+        print('Zero point energy (Eh) : {}'.format(zero_value))
+        print('G-E(el) (Eh) : {}'.format(g_value))
+        output.write(f'Final single point energy (Eh) : {all_energies[-1]}\n')
+        output.write(f'Zero point energy (Eh) : {zero_value}\n')
+        output.write(f'G-E(el) (Eh) : {g_value}\n')
+        
+        #Printing the first three vibrational frequencies
+    
+        for linenum, line in enumerate(data):
+            if 'Scaling factor for frequencies' in line:
+                scaling_line = linenum
+    
+        vibration_lines = data[scaling_line+8:scaling_line+11]
+    
+        vibration_values = []
+
+        for line in vibration_lines:
+            vibration_line_split = line.split()
+            values = vibration_line_split[1]
+            vibration_values.append(values)
+    
+        joined_values = ", ".join(vibration_values)
+    
+        print('First 3 vibrational frequencies (cm**-1) : {}'.format(joined_values))
+        output.write(f'First 3 vibrational frequencies (cm**-1) : {joined_values}\n')
+        
+        #Printing number of atoms
+    
+        for linenum, line in enumerate(data):
+            if '*xyz' in line:
+                xyz_line = linenum
+            if '****END OF INPUT****' in line:
+                end_line = linenum
+        first_atom = xyz_line + 2
+        last_atom = end_line - 1
+        num_atom = last_atom - first_atom
+        
+        #Printing molecular orbitals
+    
+        orbital_lines = []
+        mulliken_lines = []
+    
+        for linenum, line in enumerate(data):
+            if 'ORBITAL ENERGIES' in line:
+                orbital_line = linenum
+                orbital_lines.append(orbital_line)
+            if '* MULLIKEN POPULATION ANALYSIS *' in line:
+                mulliken_line = linenum
+                mulliken_lines.append(mulliken_line)
+    
+        final_orbital_line = orbital_lines[-1]
+        final_mulliken_line = mulliken_lines[-1]
+    
+        orbital_list = data[final_orbital_line+4:final_mulliken_line-2]
+    
+        occupancy_list = []
+        orbital_energies_list = []
+    
+        for line in orbital_list:
+            orbital_list_split = line.split()
+            occupancy_values = float(orbital_list_split[1])
+            occupancy_list.append(occupancy_values)
+            orbital_energies_values = float(orbital_list_split[3])
+            orbital_energies_list.append(orbital_energies_values)
+        
+        occupied_list = []
+    
+        for number in occupancy_list:
+            if number == 2:
+                occupied_list.append(number)
+    
+        length_occupied_list = len(occupied_list)
+    
+        HOMO = orbital_energies_list[length_occupied_list-1]
+        LUMO = orbital_energies_list[length_occupied_list]
+        gap = LUMO - HOMO
+    
+        print('Energy HOMO (eV) : {} / MO nº: {}'.format(HOMO, length_occupied_list-1))
+        print('Energy LUMO (eV) : {} / MO nº: {}'.format(LUMO, length_occupied_list))
+        print('gap HOMO-LUMO (eV) : {}'.format(gap))
+        output.write(f'Energy HOMO (eV) : {HOMO} / MO nº: {length_occupied_list-1}\n')
+        output.write(f'Energy LUMO (eV) : {LUMO} / MO nº: {length_occupied_list}\n')
+        output.write(f'gap (eV) : {gap}\n')
+        
+         #Ploting energies vs number of cycles 
+    
+        all_cycles = []
+    
+        for line in data:
+            if 'GEOMETRY OPTIMIZATION CYCLE' in line:
+                cycle_line = line
+                cycle_line_split = cycle_line.split()
+                numbers = float(cycle_line_split[-2])
+                all_cycles.append(numbers)
+    
+        plt.figure()
+        plt.xlabel('Number of cycles')
+        plt.ylabel('Energy (Eh)')
+        plt.plot(all_cycles, all_energies[:-1], '--o')
+        plt.savefig(F'{fname}.png')
+        
+        output.close()
+        
+    
+ 
+            
+   
+
+        
+        
+    
+            
+
+    
+
+    
